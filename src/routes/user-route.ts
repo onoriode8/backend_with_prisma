c3268ddd79ad72c0ca5efe07bf3826282d98e20f
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-16 16:14:42 +0100

    imported createPosts file

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -2,16 +2,17 @@ import { Router } from 'express'
 
 import AuthMiddleware from '../middleware/auth.middleware';
 import { createUser, queryUser, getSingleUser } from '../controller/user-controller'
+import { createPosts } from '../controller/post-controller';
 
 const router = Router();
 
 
 router.post("/create", AuthMiddleware, createUser);
 
-router.get("/query", AuthMiddleware, queryUser)
+router.get("/query", AuthMiddleware, queryUser);
 
-router.get("/query/:id", getSingleUser)
+router.get("/query/:id", getSingleUser);
 
-
+router.post("/create/posts", AuthMiddleware, createPosts);
 
 export default router;
