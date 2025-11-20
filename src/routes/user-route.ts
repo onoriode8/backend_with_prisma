31125ba8aa8149f9cd129851235da0f4748076cc
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-20 07:37:02 +0100

    added zod route validation

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express'
 
-import AuthMiddleware from '../middleware/auth.middleware';
-import { createUser, queryUser, getSingleUser } from '../controller/user-controller'
+import AuthMiddleware from '../middleware/auth/auth.middleware';
 import { createPosts } from '../controller/post-controller';
+import { LoginUserSchema } from '../schema/auth/login.user.schema'
+import { CreateUserSchema } from '../schema/auth/create.user.schema'
+import Validation from '../middleware/zod.validation/zod.validation.middleware'
+import { createUser, LoginUser, queryUser, getSingleUser } from '../controller/user-controller'
+
 
 const router = Router();
 
 
-router.post("/create", AuthMiddleware, createUser);
+router.post("/create/user", Validation(CreateUserSchema), createUser);
 
-router.get("/query", AuthMiddleware, queryUser);
+router.post("/login/user", Validation(LoginUserSchema), LoginUser);
+
+router.get("/query", AuthMiddleware, queryUser); // comment later or delete.
 
 router.get("/query/:id", getSingleUser);
 
