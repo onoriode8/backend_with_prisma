1b6a364b795db79a43e413faf9b1d5195a1c261f
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-21 08:27:52 +0100

    commented out unuse routes

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 import AuthMiddleware from '../middleware/auth/auth.middleware';
-import { createPosts } from '../controller/post-controller';
+// import { createPosts } from '../controller/post-controller';
 import { LoginUserSchema } from '../schema/auth/login.user.schema'
 import { CreateUserSchema } from '../schema/auth/create.user.schema'
 import Validation from '../middleware/zod.validation/zod.validation.middleware'
@@ -19,6 +19,6 @@ router.get("/query", AuthMiddleware, queryUser); // comment later or delete.
 
 router.get("/query/:id", getSingleUser);
 
-router.post("/create/posts", AuthMiddleware, createPosts);
+// router.post("/create/posts", AuthMiddleware, createPosts);
 
 export default router;
