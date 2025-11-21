0e24ab0c41e2e6e845fdeffa420fab19ff837ce2
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-21 08:23:15 +0100

    ignore

--- src/controller/post-controller.ts
+++ src/controller/post-controller.ts
@@ -1,29 +1,29 @@
-import { RequestHandler } from 'express'
+// import { RequestHandler } from 'express'
 
 
-import prisma from '../config/prisma'
+// import prisma from '../config/prisma'
 
 
 
-export const createPosts: RequestHandler = async(req, res) => {
-    const title = req.body.title as string
-    const paramId = req.params.id as string
-    const description = req.body.description as string
+// export const createPosts: RequestHandler = async(req, res) => {
+//     const title = req.body.title as string
+//     const paramId = req.params.id as string
+//     const description = req.body.description as string
 
-    const userId = Number(paramId)
-    try {
-        const user = await prisma.user.findFirst({
-            where: { id: userId }
-        })
+//     const userId = Number(paramId)
+//     try {
+//         const user = await prisma.user.findFirst({
+//             where: { id: userId }
+//         })
 
-        if(!user) {
-            return res.status(404).json("User not found to create a post.")
-        }
+//         if(!user) {
+//             return res.status(404).json("User not found to create a post.")
+//         }
 
-        const post = await prisma.posts.create({
-            data: { title, description }
-        })
-    } catch(err) {
-        return res.status(500).json("Something went wrong.")
-    }
-}
+//         const post = await prisma.posts.create({
+//             data: { title, description }
+//         })
+//     } catch(err) {
+//         return res.status(500).json("Something went wrong.")
+//     }
+// }
