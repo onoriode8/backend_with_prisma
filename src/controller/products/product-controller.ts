80d2d7b7e7e1480ed1f3347417c5d8969f8aa712
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-26 16:37:14 +0100

    create products controller for creating product

---
+++ src/controller/products/product-controller.ts
@@ -0,0 +1,63 @@
+import { RequestHandler } from 'express'
+
+
+import prisma from '../../config/prisma'
+import { CreateProductParamsType, CreateProductBodyType } from '../../schema/product/create.product.schema'
+
+
+
+export const createProduct: RequestHandler<CreateProductParamsType, {},  CreateProductBodyType, {}> = async (req, res) => {
+    const userId = req.params.userId;
+    const { price, quantity, productName } = req.body
+
+    if(Number(userId) !== req.userData?.userId) {
+        return res.status(401).json("Access Denied.")
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: req.userData?.userId }
+        });
+
+        if(!user) {
+            return res.status(404).json("User not found.")
+        }
+
+        const product = await prisma.products.create({
+            data: {
+                price, 
+                quantity, 
+                productName,
+                creator: {
+                    connect: {
+                        id: user.id
+                    }
+                }
+            },
+            include: {
+                creator: {
+                    select: {
+                        id: true, role: true,
+                        name: true, email: true,
+                        username: true
+                    }
+                }
+            }
+        })
+        
+        if(!product) {
+            return res.status(400).json("Failed to create product.")
+        }
+
+        res.status(200).json(product)
+    } catch(err) {
+        console.error(err)
+        return res.status(500).json("Something went wrong")
+    }
+
+}
+
+
+export const queryUserProductById: RequestHandler<{}, {}, {}, {}> = async (req, res) => {
+
+}
