e4d34fca0470125e78a0da42154c01c67749f564
Onoriode <onoriodeumukoro8@gmail.com>
2025-11-26 16:42:58 +0100

    bug fixed for error handling

--- src/middleware/auth/auth.middleware.ts
+++ src/middleware/auth/auth.middleware.ts
@@ -1,29 +1,42 @@
 import { RequestHandler } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken'
 
-// import Users from '../'
+import prisma from '../../config/prisma';
 
 
-const AuthMiddleware: RequestHandler = (req, res, next) => {
+const AuthMiddleware: RequestHandler = async (req, res, next) => {
     try {
-        // const token = req.headers.authorization?.split(" ")[1];
-        // if(!token) {
-        //     return res.status(400).json("Token not provided.")
-        // }
-        // const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload
-        // if(!decodedToken) {
-        //     return res.status(400).json("Token not provided.")
-        // }
-        // // Users.
-        req.userData = {
-            id: 123,
-            name: "PrismaPractice"
+        const token = req.headers.authorization?.split(" ")[1];
+        if(!token) {
+            return res.status(400).json("Token not provided.")
         }
-        console.log("AUTH-MIDDLEWARE")
-        next()
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload
+        if(!decodedToken) {
+            return res.status(400).json("Token not provided.")
+        }
+        const user = await prisma.user.findFirst({
+            where: { id: decodedToken.userId }
+        });
+
+        if(!user) {
+            return res.status(401).json("Unauthorize access")
+        }
+
+        req.userData = {
+            role: decodedToken.role,
+            userId: decodedToken.userId,
+            email: decodedToken.email,
+            username: decodedToken.username
+        }
+        
+        next();
     } catch(err: any) {
-        console.error(err.message)
+        if(err.message === "invalid signature") {
+            return res.status(401).json("Unauthorize user")
+        }
+        return res.status(500).json("Something went wrong.")
     }
 }
+
 
 export default AuthMiddleware;
