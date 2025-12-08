cbf98a72469677d7bb5b9b6548da75e27e15c14d
Onoriode <onoriodeumukoro8@gmail.com>
2025-12-08 09:57:04 +0100

    bug fixed

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -4,6 +4,7 @@ import 'express';
 declare module 'express-serve-static-core' {
     interface Request {
         userData?: {
+            name: string,
             role: string,
             userId: number,
             email: string,
