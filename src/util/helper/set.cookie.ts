fe05b83d81d75ce3404394cbd7cbb9690df2e6f9
Onoriode <onoriodeumukoro8@gmail.com>
2025-12-08 10:04:12 +0100

    logic for signing new accessToken and refreshToken to user

--- src/util/helper/set.cookie.ts
+++ src/util/helper/set.cookie.ts
@@ -3,7 +3,7 @@ import { Response } from "express"
 
 export function AccessToken(response: Response, accessToken: string) {
     response.cookie("accessToken", accessToken, {
-        secure: false,
+        secure: true,
         httpOnly: true,
         sameSite: "strict",
         maxAge: 1000 * 60 * 60 * 24 // 1day
@@ -12,11 +12,23 @@ export function AccessToken(response: Response, accessToken: string) {
 
 
 export function RefreshToken(response: Response, refreshToken: string) {
+
     response.cookie("refreshToken", refreshToken, {
-        secure: false,
+        secure: true,
         httpOnly: true,
         sameSite: "strict",
         maxAge: 1000 * 60 * 60 * 24 * 7, // 7days
-        path: "/user/get/data/api/refresh"
+        path: "/user/get/data/api/access/token/expires"  //"/user/get/data/api/refresh"
+    })
+}
+
+
+export const clearUserCookie = (res: Response, value: string, expires: number, path: string) => {
+    res.clearCookie(value, {
+        secure: true,
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: expires, // 7days
+        path: path    //"/user/get/data/api/refresh"
     })
 }
