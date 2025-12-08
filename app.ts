31061eaf716f4d819086158e38d35121b880ef75
Onoriode <onoriodeumukoro8@gmail.com>
2025-12-08 09:47:54 +0100

    added helmet for extra security

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
 import cors from "cors"
+import helmet from 'helmet'
+import session from 'express-session'
+import cookieParser from 'cookie-parser'
 import express, { Request, Response, NextFunction } from 'express'
 
 import userRoutes from './src/routes/user-route'
@@ -7,8 +10,26 @@ import userRoutes from './src/routes/user-route'
 const app = express()
 
 app.use(express.json())
+app.use(helmet())
+app.use(cookieParser()) // helped to parsed incoming cookie.
 app.use(cors({
-    origin: '*', // add frontend url later
+    origin: 'http://localhost:5173', // add frontend url later
+    credentials: true
+}))
+
+app.set("trust proxy", 1) // set if behind a proxy server, like AWS, Heroku, etc
+
+app.use(session({
+    // store: ,
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: true, // set to true on production
+        maxAge: 1000 * 60 * 60 * 24, // set expire time to 1 day
+        httpOnly: true,
+        sameSite: "strict",
+    }
 }))
 
 app.use("/user", userRoutes);
