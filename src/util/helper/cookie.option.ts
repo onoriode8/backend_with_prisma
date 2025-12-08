e402eaf25ed57458f1ed39bb59748c7988c4af39
Onoriode <onoriodeumukoro8@gmail.com>
2025-12-08 21:45:06 +0100

    added cookie option on a seperate services file

---
+++ src/util/helper/cookie.option.ts
@@ -0,0 +1,14 @@
+
+
+
+export const accessTokenValue = "accessToken"
+
+export const refreshTokenValue = "refreshToken"
+
+export const accessTokenExpires = 1000 * 60 * 60 * 24
+
+export const refreshTokenExpires = 1000 * 60 * 60 * 24 * 7
+
+export const accessTokenPath = "/"
+
+export const refreshTokenPath = "/user/get/data/api/refresh"
