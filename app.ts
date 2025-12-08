01fae7554c285661b3474f30f17127655633ca2b
Onoriode <onoriodeumukoro8@gmail.com>
2025-12-08 21:50:42 +0100

    added logic for universal error handling

--- app.ts
+++ app.ts
@@ -11,6 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(helmet())
+// app.use(compression())
 app.use(cookieParser()) // helped to parsed incoming cookie.
 app.use(cors({
     origin: 'http://localhost:5173', // add frontend url later
@@ -34,6 +35,10 @@ app.use(session({
 
 app.use("/user", userRoutes);
 
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    console.log("ERROR FROM UNIVERSAL ERROR MIDDLEWARE", error);
+    return res.status(400).json("Something went wrong");
+})
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     return res.status(400).json("Page not found.");
